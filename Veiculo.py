7061d22c9cfa59cbaa204f16b61dc704d3002767
DanMazzaro <danielmazzaro44@gmail.com>
2025-09-18 08:26:51 -0300

    Veiculo.py

--- Veiculo.py
+++ Veiculo.py
@@ -1,13 +1,14 @@
 class Veiculo:
     def __init__(self, marca, modelo, ano):
-        self.__marca = marca
-        self.__modelo = modelo
-        self.__ano = ano
+        self._marca = marca
+        self._modelo = modelo
+        self._ano = ano
 
     def exibir_informacoes(self):
-        return f'marca {self.__marca}, modelo {self.__modelo}, ano: {self.__ano}'
+        return f'marca {self._marca}, modelo {self._modelo}, ano: {self._ano}'
     
     def ligar(self):
-        return f'modelo {self.__modelo} esta ligado'
+        return f'modelo {self._modelo} esta ligado'
     def desligar(self):
-        return f'modelo {self.__modelo} esta desligado'
+
+        return f'modelo {self._modelo} esta desligado'
