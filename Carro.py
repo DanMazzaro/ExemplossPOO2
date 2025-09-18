f31a08f440cf4e0ca526eb9844bc5b892b62df47
DanMazzaro <danielmazzaro44@gmail.com>
2025-09-18 08:27:20 -0300

    Carro.py

--- Carro.py
+++ Carro.py
@@ -3,11 +3,12 @@ from Veiculo import Veiculo
 class Carro(Veiculo):
     def __init__(self, marca, modelo, ano, portas):
         super().__init__(marca, modelo, ano)
-        self.__portas = portas
+        self._portas = portas
 
     def abrir_porta(self):
         return f'a porta do {self._modelo} foi aberta'
 
     def exibir_informacoes(self):
         info_veiculo = super().exibir_informacoes()
+
         return f'{info_veiculo}, portas {self._portas}'
